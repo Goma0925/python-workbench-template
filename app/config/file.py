42fd58d33d95a43aa7e7865463513b4ef9f1700b
Amon Otsuki <aotsuki@meteomatics.com>
2025-02-19 10:44:44 +0100

    File input folder

--- app/config/file.py
+++ app/config/file.py
@@ -4,5 +4,8 @@ from pathlib import Path
 INPUT_DIR = Path(__file__).parent.parent.parent.joinpath("input")
 OUTPUT_DIR = Path(__file__).parent.parent.parent.joinpath("output")
 
+if not INPUT_DIR.exists():
+    INPUT_DIR.mkdir()
+
 if not OUTPUT_DIR.exists():
     OUTPUT_DIR.mkdir()
